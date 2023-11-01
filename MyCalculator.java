8cfd367041de4bd54389506d4c53d641fe72f6d8
ImDwivedi1 <Himanshudubey481@gmail.com>
2023-11-01 11:20:35 +0530

    color

--- MyCalculator.java
+++ MyCalculator.java
@@ -41,7 +41,7 @@ super(frameText);
 int tempX=TOPX, y=TOPY;
 displayLabel.setBounds(tempX,y,240,HEIGHT);
 displayLabel.setBackground(Color.VIOLET);
-displayLabel.setForeground(Color.BLUE);
+displayLabel.setForeground(Color.GREEN);
 add(displayLabel);
 
 memLabel.setBounds(TOPX,  TOPY+HEIGHT+ V_SPACE,WIDTH, HEIGHT);
